2a1189f08a1c3819c5c1449e143b3c771a5acaee
ahmed <ahmadghnnam60@gmail.com>
2024-07-18 18:53:38 +0300

    final commit

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zq76mx7f',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.ts
@@ -0,0 +1,22 @@
+// Then import schema types from any plugins that might expose them
+import works from './works'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+import testimonials from "./testimonials"
+import abouts from './abouts'
+
+export const schemaTypes = [
+    testimonials,
+    abouts, 
+    works,
+    brands,
+    experiences,
+    skills,
+    workExperience,
+    contact
+]
